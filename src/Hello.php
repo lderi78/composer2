fe4b76f9c9a24adff1d7aed4bc24cd8d6dca9b9b
Lionel D <lionel.deri@gmail.com>
2023-10-25 09:14:20 +0200

    special pour Elise :)

--- src/Hello.php
+++ src/Hello.php
@@ -14,6 +14,24 @@ public function speak(): string {
         return $this->hi;
 }
 
+/**
+ * Get the value of hi
+ */ 
+public function getHi()
+{
+return $this->hi;
 }
 
-?>
+/**
+ * Set the value of hi
+ *
+ * @return  self
+ */ 
+public function setHi($hi)
+{
+$this->hi = $hi;
+
+return $this;
+}
+
+}
